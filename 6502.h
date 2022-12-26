1a250f12083e58ad0b2893f6c0ca57cecc2172ba
Hassam Sulehria <sulehria.hassam@gmail.com>
2022-12-25 19:48:09 -0500

    Added some stuff into the CPU class

--- 6502.h
+++ 6502.h
@@ -13,14 +13,18 @@ public:
 	p6502 ();
 	~p6502 ();
 
+	void emulateCycle(); // 
+
 private:
 	// Registers
 	u8 X; // Index Register X
 	u8 Y; // Index Register Y
 	u8 A; // Accumulator
-	u8 P; // Processor Status
+	u8 P; // Processor Status NV-BDIZC
 	u16 PC; // Program Counter
 	u8 SP; // no stack overflow detection
+
+	uint8_t address_mode; // Addressing mode of the device
 
 	// Memory
 	// Maybe should make class or struct dedicated to this but for now...
